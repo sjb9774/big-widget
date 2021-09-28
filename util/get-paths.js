6f1d627e0a7f637ebb094ac89bbc0b2ec6e0290c
Stephen Biston <sbiston@trellis.co>
2021-09-28 11:48:42 -0500

    Allow directory arg for 'push'

--- util/get-paths.js
+++ util/get-paths.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = argv => {
-    const cwd = process.cwd()
-    const configFilePath = path.resolve(cwd, `config.${argv.$0}.json`)
-    const secretsFilePath = path.resolve(cwd, `secrets.${argv.$0}.json`)
+    const moduleName = path.basename(argv.$0);
+    const cwd = argv.directory ? path.resolve(argv.directory) : process.cwd()
+    const configFilePath = path.resolve(cwd, `config.${moduleName}.json`)
+    const secretsFilePath = path.resolve(cwd, `secrets.${moduleName}.json`)
     const schemaFilePath = path.resolve(cwd, 'schema.json')
     const storefrontApiQueryFilePath = path.resolve(cwd, 'storefront-api-query.graphql')
     const templateFilePath = path.resolve(cwd, 'template.html')
